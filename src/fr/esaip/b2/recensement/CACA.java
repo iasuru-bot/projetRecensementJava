cb3507c799ce69b17e7343fcb498d7ab6be6d59f
Mat <pmatteo032002@gmail.com>
2022-01-19 19:03:02 +0100

    maj

--- src/fr/esaip/b2/recensement/CACA.java
+++ src/fr/esaip/b2/recensement/CACA.java
@@ -1,4 +1,13 @@
 package fr.esaip.b2.recensement;
 
 public class CACA {
+<<<<<<< HEAD
 }
+=======
+    public static void main(String[] args) {
+        System.out.println("Matteo pue le caca");
+        System.out.println("Nan c'est Axel");
+
+    }
+}
+>>>>>>> origin/dev
