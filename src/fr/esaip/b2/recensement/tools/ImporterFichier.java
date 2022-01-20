4d5f30d137e3487bb3163136bfdf8cc246fcf285
Mat <pmatteo032002@gmail.com>
2022-01-20 14:55:54 +0100

    ImporterFichier.java récupération du fichier

--- src/fr/esaip/b2/recensement/tools/ImporterFichier.java
+++ src/fr/esaip/b2/recensement/tools/ImporterFichier.java
@@ -1,11 +1,12 @@
 package fr.esaip.b2.recensement.tools;
+import java.io.IOException;
+import java.nio.file.*;
 
 public class ImporterFichier {
-    public static void main(String[] args) {
-        Path path= Paths.get("TDNot-Java/src/recensement_2016.csv");
-        boolean estLisible = Files.isReadable(path);
-        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-        System.out.println(estLisible+lines);
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("src/recensement_2016.csv");
+        /*boolean estLisible = Files.isReadable(path);
+        System.out.println("Bon chemin : "+estLisible);*/ // c'est un test pour vérifier que le fichier sois bien lisible
     }
 
 }
