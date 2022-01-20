fe2416cd3d7551a7bd53fa9700236d1dad80dcd8
Mat <pmatteo032002@gmail.com>
2022-01-20 15:34:22 +0100

    Importerrecensement.java modification du fichier + commentaires pour eviter des erreurs

--- src/fr/esaip/b2/recensement/entities/Recensement.java
+++ src/fr/esaip/b2/recensement/entities/Recensement.java
@@ -1,11 +1,16 @@
 package fr.esaip.b2.recensement.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Classe regroupant juste une liste de villes de toutes les villes. Attention au constructeur!
 // et peut etre méthode pour ajouter une ville à la liste.
 public class Recensement {
+    private List<Ville> villes = new ArrayList<>();
+    public List<Ville> getVilles() { return villes; }
+    public void setVilles(List<Ville> villes) {
+        this.villes = villes;
 
-
-
-    //TO DO: liste de villes
-
+        //TO DO: liste de villes
+    }
 }
---
+++ src/fr/esaip/b2/recensement/tools/ImporterRecensement.java
@@ -0,0 +1,44 @@
+package fr.esaip.b2.recensement.tools;
+import fr.esaip.b2.recensement.entities.Recensement;
+import fr.esaip.b2.recensement.entities.Ville;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.List;
+
+public class ImporterRecensement {
+    public static void main(String[] args){
+    }
+
+   /* public Recensement CreationRecensement() throws IOException {
+        Path path = Paths.get("src/recensement_2016.csv");
+        boolean estLisible = Files.isReadable(path);
+        // pour vérifier les lignes qui ressortent
+        List<String> lignes = Files.readAllLines(path, StandardCharsets.UTF_8);
+        /*System.out.println(lignes);
+        System.out.println("Bon chemin : "+estLisible);
+
+        for (String ligne:lignes) {
+            // On commence par découper la ligne en morceaux sur la base du caractère séparateur « ; » .
+            // De plus on ne récupère que les morceaux qui nous intéressent.
+            // En l’occurrence on ignore les morceaux 3 et 4 dont on a pas besoin dans le TP
+
+            String[] morceaux = ligne.split(";");
+            String codeRegion = morceaux[0];
+            String nomRegion = morceaux[1];
+            String codeDepartement = morceaux[2];
+            String codeCommune = morceaux[5];
+            String nomCommune = morceaux[6];
+            String population = morceaux[7];
+
+            // Pour la population, avant la conversion en int, il faut d’abord supprimer les espaces qui se trouvent à l’intérieur.
+
+            int populationTotale = Integer.parseInt(population.replace(" ", "").trim());
+
+            // On cree maintenant la ville avec toutes ses données utiles
+
+            //Ville ville = new Ville(codeRegion, nomRegion, codeDepartement, codeCommune, nomCommune, populationTotale);
+        }
+    }*/
+}
