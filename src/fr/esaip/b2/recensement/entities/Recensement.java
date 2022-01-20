ea27f2232fd7661fa16b64a28d4cb4a8f1846a50
Mat <pmatteo032002@gmail.com>
2022-01-20 14:21:44 +0100

    ImporterFichier.java ajout vérification des liens + ajout fichier csv

--- src/fr/esaip/b2/recensement/entities/Recensement.java
+++ src/fr/esaip/b2/recensement/entities/Recensement.java
@@ -1,11 +1,11 @@
 package fr.esaip.b2.recensement.entities;
 
 //Classe regroupant juste une liste de villes de toutes les villes. Attention au constructeur!
-//importer le fichier csv ici
 // et peut etre méthode pour ajouter une ville à la liste.
 public class Recensement {
 
-    
+
+
     //TO DO: liste de villes
 
 }
--- src/fr/esaip/b2/recensement/tools/ImporterFichier.java
+++ src/fr/esaip/b2/recensement/tools/ImporterFichier.java
@@ -1,4 +1,11 @@
 package fr.esaip.b2.recensement.tools;
 
 public class ImporterFichier {
+    public static void main(String[] args) {
+        Path path= Paths.get("TDNot-Java/src/recensement_2016.csv");
+        boolean estLisible = Files.isReadable(path);
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        System.out.println(estLisible+lines);
+    }
+
 }
